b29296c16e6247eb1b7199d6510f88dd17d827dd
tientle84 <alctdl00@gmail.com>
2022-03-23 18:02:27 -0400

    update navbar add username

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -17,5 +18,12 @@ export class NavComponent implements OnInit {
 
   public logout() {
     this.userService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  public getUserName() {
+    const fistName = this.userService.getUser().firstName;
+    const lastName = this.userService.getUser().lastName;
+    return fistName + ' ' + lastName;
   }
 }
